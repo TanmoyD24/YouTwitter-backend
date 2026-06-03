407b21357d5476a459a7e2b2eb3c67b60dba5adb
TanmoyD24 <dastanmoy200224@gmail.com>
2026-06-03 11:44:42 +0530

    Add user channel profile and watch history retrieval functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshToken = async (userId) => {
@@ -339,6 +340,127 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         )
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase(),
+            },
+        },
+        {
+            $lookup: {
+                from: "subcriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subcribers",
+            },
+        },
+        {
+            $lookup: {
+                from: "subcriptions",
+                localField: "_id",
+                foreignField: "subcriber",
+                as: "subcribedTo",
+            },
+        },
+        {
+            $addFields: {
+                subcribersCount: {
+                    $size: "$subcribers",
+                },
+                channelSubcribedToCount: {
+                    $size: "$subcribedTo",
+                },
+                isSubcribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subcribers"] },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subcribersCount: 1,
+                channelSubcribedToCount: 1,
+                avatar: 1,
+                isSubcribed: 1,
+                coverImage: 1,
+                email: 1
+            },
+        },
+    ]);
+
+    if (!channel?.lenght) {
+        throw new ApiError(404, " Channel does not exists")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "User channel feteched successfully")
+        )
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            form: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user[0].watchHistory,
+        "Watch history fetched successfully")
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -349,4 +471,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
